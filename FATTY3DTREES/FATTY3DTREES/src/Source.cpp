b1cc10d80948eaf8e774ad84efe65bb6e4601516
Cladorhiza <80283120+Cladorhiza@users.noreply.github.com>
2023-12-16 19:03:40 +0000

    removed circle drawing from main
    it was so ugly

--- FATTY3DTREES/FATTY3DTREES/src/Source.cpp
+++ FATTY3DTREES/FATTY3DTREES/src/Source.cpp
@@ -24,8 +24,9 @@
 
 #define WIDTH 1280
 #define HEIGHT 720
-
-//#define DRAW_CIRCLES
+#define CLIP_NEAR 0.1f
+#define CLIP_FAR 1000.f
+#define VFOV 70.f
 
 GLFWwindow* g_window;
 
@@ -95,12 +96,12 @@ int main()
     float cameraSpeed { 0.001f };
     float cameraRotationSpeed { 0.05f};
 
-    glm::mat4 projectionMatrix { glm::perspective(glm::radians(45.f), static_cast<float>(WIDTH) / HEIGHT, 0.1f, 1000.f) };
+    glm::mat4 projectionMatrix { glm::perspective(glm::radians(VFOV), static_cast<float>(WIDTH) / HEIGHT, CLIP_NEAR, CLIP_FAR) };
     glm::mat4 viewMatrix { mainCamera.GetViewMatrix() };
     
-    std::string axiom = "X";
-    std::string rules = "X-FAECXDCBGXDBGFCBGFXDAEX,F-FF,A-A,B-B,C-C,D-D,E-E,G-G";
-    std::string turtleRules = "F-F 0.025,A-+ 25, B-- 25,C-[,D-],E-& 25,G-^ 25";
+    std::string axiom = "F";
+    std::string rules = "F-FAG,G-FBG,A-A,B-B";
+    std::string turtleRules = "F-F 0.025,G-F 0.025,A-- 90, B-+ 90";
 
     int generation = 1;
     float maxBranchRadius = 0.05f;
@@ -114,14 +115,8 @@ int main()
     unsigned vao = GLUtil::buildVAOfromData(data);
     ResourceManager::AddBuffer(vao);
 
-    bool renderCircles = false;
-
-    std::vector<unsigned int> circleVaos;
-
     Shader shader("res/shaders/BasicShader.txt");
-    Shader shader2("res/shaders/BasicShaderGeometry.txt");
     shader.Bind();
-    bool useGeoShader = false;
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(g_window))
     {
@@ -154,78 +149,28 @@ int main()
         shader.SetUniformMat4f("projectionMatrix", projectionMatrix);
         shader.SetUniformMat4f("viewMatrix", viewMatrix);
 
-        
-#ifdef DRAW_CIRCLES
-        if (InputManager::GetKeyToggle(GLFW_KEY_C))
-            renderCircles = !renderCircles;
-        if (InputManager::GetKeyToggle(GLFW_KEY_X)) {
-            useGeoShader = !useGeoShader;
-            if (useGeoShader) shader2.Bind();
-            else shader.Bind();
-        }
-#endif
         if (InputManager::GetKeyToggle(GLFW_KEY_2)) {
             generation++;
             rulesAtGeneration = LSystem::CalculateLSystemAtGeneration(axiom, rules, generation);
-            std::cout << "L-System at generation: " << generation << ": " << rulesAtGeneration << std::endl;
             turtle.Reset();
-            GraphicsTurtle::renderData t = turtle.GenerateGeometryOfLSystemRuleString(rulesAtGeneration);
+            data = turtle.GenerateGeometryOfLSystemRuleString(rulesAtGeneration);
             ResourceManager::RemoveBuffer(vao);
-            vao = GLUtil::buildVAOfromData(t);
+            vao = GLUtil::buildVAOfromData(data);
             ResourceManager::AddBuffer(vao);
-            std::cout << t.vertexes.size() << std::endl;
+            std::cout << "L-System length at generation: " << generation << ": " << data.vertexes.size() << std::endl;
             
-#ifdef DRAW_CIRCLES
-            for (const unsigned int v : circleVaos) {
-                ResourceManager::RemoveBuffer(v);
-            }
-            circleVaos.clear();
-
-            for (int i = 0; i < t.indexes.size()-1; i+=2) {
-                float radius = std::max(maxBranchRadius / (t.vertexes[t.indexes[i]].branchCount + 1), minBranchRadius);
-                float radius2 = 0.0f;
-                if (!t.vertexes[t.indexes[i + 1]].isLeaf)
-                    radius2 = std::max(maxBranchRadius / (t.vertexes[t.indexes[i+1]].branchCount + 1), minBranchRadius);
-                float cylinderColour[4]{brown[0],brown[1],brown[2],brown[3]};
-                if (radius == minBranchRadius) {
-                    cylinderColour[0] = green[0];
-                    cylinderColour[1] = green[1];
-                    cylinderColour[2] = green[2];
-                    cylinderColour[3] = green[3];
-                }
-                unsigned int v = GLUtil::buildCylinderVAO(t.vertexes[t.indexes[i]].position, t.vertexes[t.indexes[i+1]].position, t.vertexes[t.indexes[i]].circleNormal, t.vertexes[t.indexes[i+1]].circleNormal, radius, radius2, 10, cylinderColour);
-                ResourceManager::AddBuffer(v);
-                circleVaos.push_back(v);
-            }
-#endif
         }
             
         if (InputManager::GetKeyToggle(GLFW_KEY_1)) {
             if (generation > 0) generation--;
             rulesAtGeneration = LSystem::CalculateLSystemAtGeneration(axiom, rules, generation);
             turtle.Reset();
-            GraphicsTurtle::renderData t = turtle.GenerateGeometryOfLSystemRuleString(rulesAtGeneration);
+            data = turtle.GenerateGeometryOfLSystemRuleString(rulesAtGeneration);
             ResourceManager::RemoveBuffer(vao);
-            vao = GLUtil::buildVAOfromData(t);
+            vao = GLUtil::buildVAOfromData(data);
             ResourceManager::AddBuffer(vao);
-#ifdef DRAW_CIRCLES
-            for (const unsigned int v : circleVaos) {
-                ResourceManager::RemoveBuffer(v);
-            }
-            circleVaos.clear();
+            std::cout << "L-System length at generation: " << generation << ": " << data.vertexes.size() << std::endl;
 
-            for (const GraphicsTurtle::vertex& p : t.vertexes)
-            {
-                float radiusRatio = (t.maximumDistanceFromOrigin - p.distanceFromOrigin) / t.maximumDistanceFromOrigin;
-                if (radiusRatio == 0.0f) 
-                    radiusRatio = 0.0001f;
-                float radius = 0.05f * radiusRatio;
-                unsigned int v = GLUtil::buildCircleVAO(p.position, p.circleNormal, radius, 10, green);
-                ResourceManager::AddBuffer(v);
-                circleVaos.push_back(v);
-            }
-            std::cout << t.vertexes.size() << std::endl;
-#endif
         }
 
         // Start the Dear ImGui frame
@@ -250,16 +195,9 @@ int main()
 
         //RENDER
         glBindVertexArray(vao);
-        glDrawElements(GL_LINES, 10000000, GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_LINES, data.indexes.size(), GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
 
-#ifdef DRAW_CIRCLES
-        if (renderCircles) {
-            for (unsigned vao : circleVaos) {
-                glBindVertexArray(vao);
-                glDrawElements(GL_TRIANGLES, 60, GL_UNSIGNED_INT, 0);
-            }
-        }
-#endif
         //draw UI over everything else
 		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
